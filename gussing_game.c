18d5b0bfc2e9fefaff62cd2f92f644efc5b964a1
KirtiJha <kirti.jha2025@gmail.com>
2025-03-22 14:28:02 +0530

    added some logic

--- gussing_game.c
+++ gussing_game.c
@@ -2,6 +2,7 @@
  int main(){
 
     int random, guess;
+    int no_of_guess = 0;
 
     printf("Welcome to the world of Gussing Numbers \n ");
 
@@ -9,14 +10,18 @@
     do{
        printf("\nPlease enter your Guess between(1 to 100): ");
        scanf("%d", &guess);
+       no_of_guess++; //this is for how many time u take to guess the write answer
 
        if(guess < random){
         printf("Guess a larger number.\n");
        }else if(guess > random){
         printf("Guess a smaller number.\n");
        }else{
-        printf("Congratulations !!! You have successfully guessed the number.\n");
+        printf("Congratulations !!! You have successfully guessed the number in %d attempts",no_of_guess);
        }
     } while (guess != random); 
     // loop will run until guess will not equal to random number  
+
+    printf("Bye Bye!, Thanks for Playing");
+    printf("Developed by: Kumari Kirti Jha");
  }
